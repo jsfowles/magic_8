26f017886d2e1e8c4593d585f20f1eb121ad0edd
Jacob Fowles <js.fowles@gmail.com>
2016-05-12 14:31:42 -0600

    the basic magic_8_ball

--- magic_8.rb
+++ magic_8.rb
@@ -1,14 +1,15 @@
 
-#get the question
-#spit out random answer
 #ablity to exit at any time
 
-@answers = {"Yes", "No", "Maybe", "Try again later"}
+@answers = ["Yes", "No", "Maybe", "Try again later"]
 
 def magic
   puts "Welcome to the Magic 8 Ball"
   puts "Type a question to receive an answer, type 'q' to Exit"
+  question = gets.strip
+  exit if question == 'q'
+  puts @answers.sample
+  magic
 end
 
-exit if gets.strip.downcase == 'q'
-asdfasdf
+magic
