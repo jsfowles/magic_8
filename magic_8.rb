153605ee825c93bf60b73cd5404d4ce33ddf6ea4
Jacob Fowles <js.fowles@gmail.com>
2016-05-12 13:50:39 -0600

    print header added

---
+++ magic_8.rb
@@ -0,0 +1,12 @@
+#print a welcome message
+#get first numner
+#get operator
+#validate correct operator
+#get second number
+#print output
+#prompt to continue or exit
+
+def print_header
+  puts "Welcome to Ruby Calculator"
+  puts "__________________________"
+end
